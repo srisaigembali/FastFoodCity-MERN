ec4783d3dee5996ca3fd160f2f1f25fc7cb4ce91
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2023-07-14 13:05:43 +0530

    added header component

--- client/src/containers/Main.jsx
+++ client/src/containers/Main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <main className='w-screen min-h-screen flex items-center justify-start flex-col bg-primary'>
+      <Header />
+    </main>
+  );
 };
 
 export default Main;
---
+++ client/src/utils/style.js
@@ -0,0 +1,5 @@
+export const isActiveStyles =
+  " text-2xl text-blue-700 font-semibold hover:text-blue-700 px-4 py-2 duration-100 transition-all ease-in-out";
+
+export const isNotActiveStyles =
+  " text-xl text-textColor hover:text-blue-700 duration-100 px-4 py-2 transition-all ease-in-out";
